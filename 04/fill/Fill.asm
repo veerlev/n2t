265bf67a292ad5e1941b6d5eb84b80692824ed06
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-15 08:53:15 +0300

    Update Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,83 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+	@formerKey
+	M=0   
+	
+	(LISTEN)
+	@KBD
+	D=M
+	@currentKey
+	M=D
+	
+	@formerKey
+	D=D-M
+	@LISTEN
+	D;JEQ
+
+	
+	@KBD
+	D=M
+
+	@BLACKEN
+	D;JNE
+
+	@WHITEN
+	D;JEQ
+
+	(BLACKEN)
+	@fillValue
+	M=-1
+	@FILL
+	0;JMP
+
+	(WHITEN)
+	@fillValue
+	M=0
+	@FILL
+	0;JMP
+
+	(FILL)
+	@i
+	M=0
+	@KBD
+	D=A
+	@end
+	M=D
+	
+	
+	(LOOP)
+		@SCREEN
+		D=A
+		@i
+		D=D+M
+		@end
+		D=M-D
+		@UPDATE
+		D;JEQ
+
+		@SCREEN
+		D=A
+		@i
+		D=D+M
+		@currentPosition
+		M=D
+		@fillValue
+		D=M
+		@currentPosition
+		A=M
+		M=D
+		@i
+		M=M+1
+	
+	
+		@LOOP
+		0;JMP
+	
+	(UPDATE)
+	@currentKey
+	D=M
+	@formerKey
+	M=D
+	@LISTEN
+	0;JMP
